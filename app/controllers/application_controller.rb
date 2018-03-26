70f834916f16c1202d1640702e4700d1039e5166
Michael Kohl <citizen428@gmail.com>
2018-03-26 16:53:42 +0700

    Add Current.user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    Current.user = User.first # normally this would be current_user
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
   has_one_attached :avatar
+
+  belongs_to :user, default: -> { Current.user }
 end
